b8eeec79a4720acbd8fc675a073588d262eb794c
RaiMan <rmhdevelop@me.com>
2017-04-08 19:15:30 +0200

    first upload

---
+++ src/main/java/com/sikulix/tigervnc/Sikulix.java
@@ -0,0 +1,7 @@
+package com.sikulix.tigervnc;
+
+public class Sikulix {
+  public static void main(String[] args) {
+    System.out.println("com.sikulix.tigervnc.Sikulix: nothing to do here");
+  }
+}
